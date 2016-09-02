2883c61a78f2f1f47b1cc28ee98d6a781e77b3e5
sum <scausum@163.com>
2016-09-02 23:57:05 +0800

    Delete RecyclerViewHolder class

--- app/src/main/java/com/fubaisum/sample/ContentDelegate.java
+++ app/src/main/java/com/fubaisum/sample/ContentDelegate.java
@@ -2,19 +2,19 @@ package com.fubaisum.sample;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.fubaisum.adapterdelegate.AbsAdapterDelegate;
 import com.fubaisum.adapterdelegate.OnDelegateClickListener;
-import com.fubaisum.adapterdelegate.RecyclerViewHolder;
 import com.fubaisum.sample.model.ContentItem;
 import com.fubaisum.sample.model.Item;
 
 /**
  * Created by sum on 5/10/16.
  */
-public class ContentDelegate extends AbsAdapterDelegate<Item, RecyclerViewHolder> {
+public class ContentDelegate extends AbsAdapterDelegate<Item, ContentDelegate.ViewHolder> {
 
     private OnDelegateClickListener onDelegateClickListener;
 
@@ -32,19 +32,42 @@ public class ContentDelegate extends AbsAdapterDelegate<Item, RecyclerViewHolder
     }
 
     @Override
-    protected RecyclerViewHolder onCreateViewHolder(View itemView) {
-        RecyclerViewHolder viewHolder = new RecyclerViewHolder(itemView);
+    protected ViewHolder onCreateViewHolder(View itemView) {
+        ViewHolder viewHolder = new ViewHolder(itemView);
         viewHolder.setOnDelegateClickListener(onDelegateClickListener);
-        viewHolder.setOnClickListener(R.id.tv_main_item_content);
         return viewHolder;
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull Item item, @NonNull RecyclerViewHolder holder) {
+    protected void onBindViewHolder(@NonNull Item item, @NonNull ViewHolder holder) {
         ContentItem contentItem = (ContentItem) item;
+        holder.tvContent.setText(contentItem.content != null ? contentItem.content : "Hello World!!!");
+    }
 
-        TextView tvContent = holder.getView(R.id.tv_main_item_content);
-        tvContent.setText(contentItem.content != null ? contentItem.content : "Hello World!!!");
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView tvContent;
+
+        private OnDelegateClickListener onDelegateClickListener;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvContent = (TextView) itemView.findViewById(R.id.tv_main_item_content);
+            tvContent.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (onDelegateClickListener != null) {
+                onDelegateClickListener.onClick(v, position);
+            }
+        }
+
+        public void setOnDelegateClickListener(OnDelegateClickListener onDelegateClickListener) {
+            this.onDelegateClickListener = onDelegateClickListener;
+        }
     }
 
 }
--- app/src/main/java/com/fubaisum/sample/ImageDelegate.java
+++ app/src/main/java/com/fubaisum/sample/ImageDelegate.java
@@ -2,18 +2,19 @@ package com.fubaisum.sample;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.fubaisum.adapterdelegate.AbsAdapterDelegate;
-import com.fubaisum.adapterdelegate.RecyclerViewHolder;
-import com.fubaisum.sample.model.Item;
+import com.fubaisum.adapterdelegate.OnDelegateClickListener;
 import com.fubaisum.sample.model.ImageItem;
+import com.fubaisum.sample.model.Item;
 
 /**
  * Created by sum on 5/10/16.
  */
-public class ImageDelegate extends AbsAdapterDelegate<Item, RecyclerViewHolder> {
+public class ImageDelegate extends AbsAdapterDelegate<Item, ImageDelegate.ViewHolder> {
 
     public ImageDelegate(Activity activity) {
         super(activity, R.layout.layout_image);
@@ -25,16 +26,40 @@ public class ImageDelegate extends AbsAdapterDelegate<Item, RecyclerViewHolder> 
     }
 
     @Override
-    protected RecyclerViewHolder onCreateViewHolder(View itemView) {
-        return new RecyclerViewHolder(itemView);
+    protected ViewHolder onCreateViewHolder(View itemView) {
+        return new ViewHolder(itemView);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull Item item, @NonNull RecyclerViewHolder holder) {
+    protected void onBindViewHolder(@NonNull Item item, @NonNull ViewHolder holder) {
         ImageItem imageItem = (ImageItem) item;
 
-        ImageView ivIcon = holder.getView(R.id.iv_main_item_icon);
-        ivIcon.setImageResource(imageItem.imageRes != 0 ? imageItem.imageRes : R.mipmap.ic_launcher);
+        holder.ivIcon.setImageResource(imageItem.imageRes != 0 ? imageItem.imageRes : R.mipmap.ic_launcher);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private ImageView ivIcon;
+
+        private OnDelegateClickListener onDelegateClickListener;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            ivIcon = (ImageView) itemView.findViewById(R.id.iv_main_item_icon);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (onDelegateClickListener != null) {
+                onDelegateClickListener.onClick(v, position);
+            }
+        }
+
+        public void setOnDelegateClickListener(OnDelegateClickListener onDelegateClickListener) {
+            this.onDelegateClickListener = onDelegateClickListener;
+        }
     }
 
 }
