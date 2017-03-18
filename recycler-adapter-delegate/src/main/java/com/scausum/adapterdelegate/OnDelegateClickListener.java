bc2eadca6bb090658ec68983a73266c1479c3542
sum <sum@localhost.localdomain>
2017-03-18 14:04:24 +0800

    refactor OnDelegateClickListener

--- recycler-adapter-delegate/src/main/java/com/scausum/adapterdelegate/OnDelegateClickListener.java
+++ recycler-adapter-delegate/src/main/java/com/scausum/adapterdelegate/OnDelegateClickListener.java
@@ -1,16 +1,39 @@
 package com.scausum.adapterdelegate;
 
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 /**
- * Click itemView's children view listener
+ *
  */
-public interface OnDelegateClickListener {
+public abstract class OnDelegateClickListener
+        implements View.OnClickListener, View.OnLongClickListener {
+
+    private RecyclerView.ViewHolder viewHolder;
+
+    public void setViewHolder(RecyclerView.ViewHolder viewHolder) {
+        this.viewHolder = viewHolder;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (viewHolder == null) {
+            return;
+        }
+        int position = viewHolder.getAdapterPosition();
+        onClick(v, position);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return true;
+    }
+
     /**
      * Callback when click the itemView's child view
      *
      * @param child    the itemView's child view
      * @param position the itemView's position in RecyclerView
      */
-    void onClick(View child, int position);
+    abstract void onClick(View child, int position);
 }
